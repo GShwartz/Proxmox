1be330414dcf2bd2511ac9813b7784a3080f0abd
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-01-11 23:41:22 +0200

    Update deploy-vms.sh

--- deploy-vms.sh
+++ deploy-vms.sh
@@ -22,5 +22,5 @@ for ((i=1;i<=$vmnum; i++)); do
 	newname="${vmname}0$(expr 0 + $i)"
 	vmid="$(expr 100 + $i)"
 	echo "Creating VM $newname..."
-	qm create $vmid --name $newname --memory $vmram --cores $corenum --ide0 local-lvm:$hdsize --net0 virtio,bridge=vmbr0 --ipconfig0 ip=192.168.100.100/24,gw=192.168.100.2 --ide1 /var/lib/vz/template/iso/ubuntu-22.04.1-live-server-amd64.iso,media=cdrom --boot c &
+	qm create $vmid --name $newname --memory $vmram --cores $corenum --ide0 local-lvm:$hdsize --net0 virtio,bridge=vmbr0 --ide1 /var/lib/vz/template/iso/ubuntu-22.04.1-live-server-amd64.iso,media=cdrom --boot c &
 done
