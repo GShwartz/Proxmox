19e10bbd1ce5b767e8d1dc7420edae0ecbe9ccc1
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-01-11 23:37:54 +0200

    Add files via upload

---
+++ deploy-vms.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Number of VMS
+read -p "Number of VMs: " vmnum
+
+# Starting VM ID
+read -p "Starting VM ID: " vmid
+
+# VM Name
+read -p "VM Name (will show VMname + VMid): " vmname
+
+# RAM for each container
+read -p "RAM for each VM in MB: " vmram
+
+# Number of Cores
+read -p "Cores: " corenum
+
+# Hard Disk Size
+read -p "HD Size: " hdsize
+
+for ((i=1;i<=$vmnum; i++)); do
+	newname="${vmname}0$(expr 0 + $i)"
+	vmid="$(expr 100 + $i)"
+	echo "Creating VM $newname..."
+	qm create $vmid --name $newname --memory $vmram --cores $corenum --ide0 local-lvm:$hdsize --net0 virtio,bridge=vmbr0 --ipconfig0 ip=192.168.100.100/24,gw=192.168.100.2 --ide1 /var/lib/vz/template/iso/ubuntu-22.04.1-live-server-amd64.iso,media=cdrom --boot c &
+done
