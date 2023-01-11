f22ecdd8e1d380c00d49a890ca42aa64b0a19d69
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-01-11 23:59:01 +0200

    Update deploy-vms.sh

--- deploy-vms.sh
+++ deploy-vms.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# VM OS
+echo "Storage Status:"
+pvesm status
+echo "================"
+echo "----========----"
+echo "ISO Images"
+ls -l /var/lib/vz/template/iso
+echo ""
+read -p "VM OS Image: " vmos
+
 # Number of VMS
 read -p "Number of VMs: " vmnum
 
@@ -19,8 +29,8 @@ read -p "Cores: " corenum
 read -p "HD Size: " hdsize
 
 for ((i=0;i<=$vmnum-1; i++)); do
-	newname="${vmname}0$(expr 0 + $i)"
+	newname="${vmname}0$(expr 1 + $i)"
 	newvmid="$(expr $vmid + $i)"
 	echo "Creating VM $newname..."
-	qm create $newvmid --name $newname --memory $vmram --cores $corenum --ide0 local-lvm:$hdsize --net0 virtio,bridge=vmbr0 --ide1 /var/lib/vz/template/iso/ubuntu-22.04.1-live-server-amd64.iso,media=cdrom --boot c &
+	qm create $newvmid --name $newname --memory $vmram --cores $corenum --ide0 local-lvm:$hdsize --net0 virtio,bridge=vmbr0 --ide1 /var/lib/vz/template/iso/$vmos,media=cdrom --boot c &
 done
