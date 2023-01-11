297e6b2e77631ac16c98eba15e4d7f1faa3b8250
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-01-11 23:48:21 +0200

    Update deploy-vms.sh

--- deploy-vms.sh
+++ deploy-vms.sh
@@ -18,9 +18,9 @@ read -p "Cores: " corenum
 # Hard Disk Size
 read -p "HD Size: " hdsize
 
-for ((i=1;i<=$vmnum; i++)); do
+for ((i=0;i<=$vmnum-1; i++)); do
 	newname="${vmname}0$(expr 0 + $i)"
-	vmid="$(expr 100 + $i)"
+	newvmid="$(expr $vmid + $i)"
 	echo "Creating VM $newname..."
-	qm create $vmid --name $newname --memory $vmram --cores $corenum --ide0 local-lvm:$hdsize --net0 virtio,bridge=vmbr0 --ide1 /var/lib/vz/template/iso/ubuntu-22.04.1-live-server-amd64.iso,media=cdrom --boot c &
+	qm create $newvmid --name $newname --memory $vmram --cores $corenum --ide0 local-lvm:$hdsize --net0 virtio,bridge=vmbr0 --ide1 /var/lib/vz/template/iso/ubuntu-22.04.1-live-server-amd64.iso,media=cdrom --boot c &
 done
